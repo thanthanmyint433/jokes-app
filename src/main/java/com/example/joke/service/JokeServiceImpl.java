959e95e56d560b5d41922d691a5aa96cd91b62e9
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-10 11:29:46 +0630

    add: spring configuration

--- src/main/java/com/example/joke/service/JokeServiceImpl.java
+++ src/main/java/com/example/joke/service/JokeServiceImpl.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
